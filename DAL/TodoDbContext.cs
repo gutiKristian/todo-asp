0de7b3962552eafed759955b4829e8b3d368673f
Kristian Gutic <kristian.gutic@gmail.com>
2023-08-02 00:12:11 +0200

    Adding DBContext

---
+++ DAL/TodoDbContext.cs
@@ -0,0 +1,32 @@
+﻿using DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class TodoDbContext : DbContext
+    {
+        public string ConnectionString { get; set; }
+
+        public DbSet<TaskType> Types { get; set; }
+        public DbSet<TaskTodo> Tasks { get; set; }
+        public DbSet<TaskCollection> TaskCollections { get; set; }
+
+        public TodoDbContext(string connectionString)
+        {
+
+            ConnectionString = connectionString;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionString);
+            base.OnConfiguring(optionsBuilder);
+        }
+
+    }
+}
