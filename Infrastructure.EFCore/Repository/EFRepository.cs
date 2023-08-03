bbe849171e396b367c3382a5702aa6ff59fd3f87
Kristian Gutic <kristian.gutic@gmail.com>
2023-08-03 23:40:16 +0200

    Init concrete EFRepository implementation

---
+++ Infrastructure.EFCore/Repository/EFRepository.cs
@@ -0,0 +1,37 @@
+﻿using Infrastructure.Repository;
+
+namespace Infrastructure.EFCore.Repository
+{
+    public class EFRepository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity GetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Insert(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
