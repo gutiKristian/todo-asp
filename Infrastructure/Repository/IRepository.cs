acbfd0bc970d8f53ae339f558e9346680ee9fe76
Kristian Gutic <kristian.gutic@gmail.com>
2023-08-03 23:33:29 +0200

    Init IRepository

---
+++ Infrastructure/Repository/IRepository.cs
@@ -0,0 +1,18 @@
+﻿namespace Infrastructure.Repository
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        TEntity GetByID(int id);
+
+        IEnumerable<TEntity> GetAll();
+
+        int Insert(TEntity entity);
+
+        void Delete(int id);
+
+        void Delete(TEntity entity);
+
+        void Update(TEntity entity);
+    }
+
+}
